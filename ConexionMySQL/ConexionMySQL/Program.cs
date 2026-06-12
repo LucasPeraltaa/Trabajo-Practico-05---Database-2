3a3d4375a5c06c8470bc2f78e68a55a91e66167e
LucasPeraltaa <lucasmperaltaa@gmail.com>
2026-06-12 20:55:53 -0300

    no tenia la carpeta del c# en la carpeta del tp, ahora la cargamos y subimos

---
+++ ConexionMySQL/ConexionMySQL/Program.cs
@@ -0,0 +1,29 @@
+﻿using MySql.Data.MySqlClient;
+using System.Data;
+
+string conexion =
+    "server=localhost;database=EmpresaPersonal;uid=root;pwd=123456789;";
+
+using (MySqlConnection conn = new MySqlConnection(conexion))
+{
+    try
+    {
+        conn.Open();
+
+        MySqlCommand cmd =
+            new MySqlCommand("InsertarPersona", conn);
+
+        cmd.CommandType = CommandType.StoredProcedure;
+
+        cmd.Parameters.AddWithValue("p_Apellido", "Martinez");
+        cmd.Parameters.AddWithValue("p_Nombre", "Pedro");
+
+        cmd.ExecuteNonQuery();
+
+        Console.WriteLine("Persona insertada correctamente");
+    }
+    catch (Exception ex)
+    {
+        Console.WriteLine(ex.Message);
+    }
+}
